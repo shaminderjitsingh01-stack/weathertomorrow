5b66dba0b2e0c0cbc1df9371f329496bdd623d3a
Shaminderjit Singh <shaminderjit@distributionnn.com>
2026-03-15 05:27:24 +0800

    Add Google Analytics (G-T9EJK952KQ)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,6 +71,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T9EJK952KQ" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-T9EJK952KQ');`,
+          }}
+        />
+      </head>
       <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
   );
